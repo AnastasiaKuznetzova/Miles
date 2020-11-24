91e4680cac710c0006440f73f297012c27479bf3
AnastasiaKuznetsova <you@example.com>
2020-11-24 22:06:01 +0300

    new

---
+++ miles.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketpriceInKopecks = 1300000;
+        int milesInKopecks = 2000;
+        int totalmiles = ticketpriceInKopecks / milesInKopecks;
+        System.out.println(totalmiles);
+    }
+}
